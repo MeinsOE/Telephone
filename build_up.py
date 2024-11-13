f9dee74c316eae852579f07ee9233ddfeb8c5699
Balderus <riekenberg@outlook.de>
2024-11-13 22:17:46 +0100

    Uses bitarray for number and implements upper number limit

--- build_up.py
+++ build_up.py
@@ -3,6 +3,7 @@ from scipy.special import binom
 from typing import List
 import pickle
 import os
+from bitarray import bitarray
 
 class Junction:
     isSymmetric:bool = False
@@ -36,15 +37,16 @@ class Divide(Junction):
         denominator = min(a, b)
         if denominator==0:
             return 1 # always already satisfied
-        return int(numerator/denominator)
+        return numerator//denominator
 
-maxPowerValue = 30
+maxPower = 30
+maxNumber = 2 ** maxPower
 
 class Power(Junction):
     isSymmetric = False
     
     def use(base, exponent):
-        if base.bit_length() * exponent > maxPowerValue:
+        if base.bit_length() * exponent > maxPower:
             return 1 # always already satisfied
         return base**exponent
 
@@ -54,7 +56,7 @@ class Binomial(Junction):
     def use(a, b):
         n = max(a, b)
         k = min(a, b)
-        if max(k, n-k).bit_length() * min(k, n-k) > maxPowerValue:
+        if max(k, n-k).bit_length() * min(k, n-k) > maxPower:
             return 1 # always already satisfied
         return int(round(binom(n, k)))
     
@@ -68,24 +70,26 @@ funcs = [Binomial, Power, Divide, Subtract, Multiply, Add]
 
 if __name__ == "__main__":
     startTime = datetime.now()
-    numbersFile = "build_up_data/numbers.pkl"
+    numbersFile = "build_up_data/numberBits.pkl"
     nodesFile = "build_up_data/nodes.pkl"
     indicesFile = "build_up_data/indices.pkl"
     if os.path.isfile(numbersFile):
         with open(numbersFile, "rb") as file:
-            numbers = pickle.load(file)
+            numberBits = pickle.load(file)
         with open(indicesFile, "rb") as file:
             startingIndices = pickle.load(file)
         with open(nodesFile, "rb") as file:
             numberNodes = pickle.load(file)
     else:
-        numbers = set([i for i in range(1, 10)])
-        numberNodes : List[List] = [[number, None, None, "root"] for number in numbers]
+        numberBits = bitarray(maxNumber)
+        for i in range(10):
+            numberBits[i] = 1
+        numberNodes : List[List] = [[number, None, None, "root"] for number in range(1, 10)]
         startingIndices = [0]
 
-    targetNumber = 1.5
+    targetNumber = 9851722
     found = False
-    if targetNumber in numbers:
+    if numberBits[targetNumber]:
         for node in numberNodes:
             if node[0] == targetNumber:
                 print(node)
@@ -94,9 +98,9 @@ if __name__ == "__main__":
         startingIndices += [len(numberNodes)]
         print(f"complexity={len(startingIndices)}")
         print(f"{startingIndices[-1]}")
-        print(int(len(startingIndices)/2))
+        print(len(startingIndices)//2)
         print()
-        for rangeIndex in range(int(len(startingIndices)/2)):
+        for rangeIndex in range(len(startingIndices)//2):
             for index1 in range(startingIndices[rangeIndex], startingIndices[rangeIndex+1]):
                 node1 = numberNodes[index1]
                 print (f"\033[A{rangeIndex+1}: {index1 - startingIndices[rangeIndex] + 1} / {startingIndices[rangeIndex+1] - startingIndices[rangeIndex]}")
@@ -105,8 +109,8 @@ if __name__ == "__main__":
                     for func in funcs:
                         if func.isSymmetric:
                             result = func.use(node1[0], node2[0])
-                            if result not in numbers:
-                                numbers.add(result)
+                            if result < maxNumber and  not numberBits[result]:
+                                numberBits[result] = 1
                                 newNode = [result, index1, index2, func.__name__]
                                 if result == targetNumber:
                                     print(newNode)
@@ -115,8 +119,8 @@ if __name__ == "__main__":
                                 numberNodes += [newNode]
                         else:
                             result = func.use(node1[0], node2[0])
-                            if result not in numbers:
-                                numbers.add(result)
+                            if result < maxNumber and  not numberBits[result]:
+                                numberBits[result] = 1
                                 newNode = [result, index1, index2, func.__name__]
                                 if result == targetNumber:
                                     print(newNode)
@@ -124,8 +128,8 @@ if __name__ == "__main__":
                                     found = True
                                 numberNodes += [newNode]
                             result = func.use(node2[0], node1[0])
-                            if result not in numbers:
-                                numbers.add(result)
+                            if result < maxNumber and  not numberBits[result]:
+                                numberBits[result] = 1
                                 newNode = [result, index2, index1, func.__name__]
                                 if result == targetNumber:
                                     print(newNode)
@@ -133,7 +137,7 @@ if __name__ == "__main__":
                                     found = True
                                 numberNodes += [newNode]
         with open(numbersFile, "wb") as file:
-            pickle.dump(numbers, file)
+            pickle.dump(numberBits, file)
         with open(nodesFile, "wb") as file:
             pickle.dump(numberNodes, file)
         with open(indicesFile, "wb") as file:
